4c6c6bc368efc471922d098a6a9c0d86039ca467
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2020-04-11 15:51:15 -0300

    feat: create get and post

--- src/app.js
+++ src/app.js
@@ -1,33 +1,45 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4")
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
 app.get("/repositories", (request, response) => {
-  // TODO
-});
+  return response.json(repositories)
+})
 
 app.post("/repositories", (request, response) => {
-  // TODO
-});
+  const { title, url, techs } = request.body
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
+})
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
-});
+})
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
-});
+})
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
-});
+})
 
-module.exports = app;
+module.exports = app
